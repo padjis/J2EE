cfa16652475b072fb199f470eca017674e55bdf8
padjis <padjis2008@yahoo.fr>
2014-02-09 22:36:26 +0000

    configuration de JPA avec nos classes

---
+++ J2eeDal/ejbModule/com/indev/worlcup/extra/Item.java
@@ -0,0 +1,61 @@
+package com.indev.worlcup.extra;
+/***********************************************************************
+ * Module:  Item.java
+ * Author:  PADJIS
+ * Purpose: Defines the Class Item
+ ***********************************************************************/
+
+
+import java.util.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+	@Id
+   private int iditem;
+   private String typei;
+   private double pricei;
+   private int quantityi;
+   private String nomi;
+   private String descriptioni;
+   
+	public int getIditem() {
+		return iditem;
+	}
+	public void setIditem(int iditem) {
+		this.iditem = iditem;
+	}
+	public String getTypei() {
+		return typei;
+	}
+	public void setTypei(String typei) {
+		this.typei = typei;
+	}
+	public double getPricei() {
+		return pricei;
+	}
+	public void setPricei(double pricei) {
+		this.pricei = pricei;
+	}
+	public int getQuantityi() {
+		return quantityi;
+	}
+	public void setQuantityi(int quantityi) {
+		this.quantityi = quantityi;
+	}
+	public String getNomi() {
+		return nomi;
+	}
+	public void setNomi(String nomi) {
+		this.nomi = nomi;
+	}
+	public String getDescriptioni() {
+		return descriptioni;
+	}
+	public void setDescriptioni(String descriptioni) {
+	this.descriptioni = descriptioni;
+}
+
+}
